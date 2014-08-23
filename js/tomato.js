6aa3771c96a3d5846799ba706a2e4a8faca88b71
andersonmadeira <andersonmadeirawired@gmail.com>
2014-08-22 21:26:36 -0300

    Classes
    Now Transition, State, Label are three classes.

--- js/tomato.js
+++ js/tomato.js
@@ -27,7 +27,7 @@ var App = {
 	insert_state: function() {
 		// if state mode is on
 		if (App.Config.Mode == 1) {
-			State.build(paper, "q"+App.Config.StateCount);
+			new State("q"+App.Config.StateCount);
 			App.Config.StateCount += 1;
 		}
 	},
@@ -59,17 +59,18 @@ Raphael.st.draggable = function() {
 		oy = 0,
 		sx = 0, // start pos
 		sy = 0;
-	moveFun = function(dx, dy) {
+
+	__move = function(dx, dy) {
 		if (App.Config.Mode == 1) {
 			lx = dx + ox;
 			ly = dy + oy;
 			me.transform('t' + lx + ',' + ly);
 		} else if (App.Config.Mode == 2) {
-			paper.path('M '+sx+','+sy+' Q 100,40 '+lx+','+ly).attr({stroke: "#000", "stroke-dasharray": "- "});
+
 		}
 	},
 	// note: also a single left mouse click
-	startFun = function(startX, startY) {
+	__start = function(startX, startY) {
 		// change opacity
 		me.animate({'fill-opacity': 0.5, 'stroke-opacity': 0.7}, 200);
 		// start dragging brings the set to front
@@ -80,7 +81,7 @@ Raphael.st.draggable = function() {
 			sy = startY;
 		}
 	},
-	endFun = function() {
+	__end = function() {
 		me.animate({'fill-opacity': 1, 'stroke-opacity': 1}, 200);
 
 		if (App.Config.Mode == 1) {
@@ -88,126 +89,126 @@ Raphael.st.draggable = function() {
 			oy = ly;
 			console.log('End drag at: '+ox+','+oy);
 		} else if (App.Config.Mode == 2) {
+			new Transition(sx, sy, lx, ly);
 		}
 	};
   
-	this.drag(moveFun, startFun, endFun);
+	this.drag(__move, __start, __end);
 };
 
 /**  
-* State Factory: responsible for building state objects
-*/
-var State = {
-	/**
-	 * @param paper: RaphaelJS canvas object.
-	 * @param paramStrText: string
-	 * @param paramX: number
-	 * @param paramY: number
-	 * @param paramRadius: number
-	 */
-	build: function(paper, paramStrText, paramX, paramY, paramRadius) {
-		var obj = new State.__stateClass();
-		// default values
-		var x       = (typeof paramX      !== 'undefined')  ? paramX      : App.Config.Pref.xPos;
-		var y       = (typeof paramY      !== 'undefined')  ? paramY      : App.Config.Pref.yPos;
-		var radius  = (typeof paramRadius !== 'undefined')  ? paramRadius : App.Config.Pref.radius;
-		// starts set, ie Circle (State) + Text (Label)
-		paper.setStart();
-		// create the object
-		obj._pCircle = paper.circle(x, y, radius);
-		obj._pCircle.attr("stroke-width", App.Config.Pref.strokeWidth);
-		obj._pCircle.attr("fill", App.Config.Pref.fillColor);
-		obj._pCircle.attr("stroke", App.Config.Pref.strokeColor);
-		obj._label = Label.build(paramStrText, 
-			obj._pCircle.attr('cx'), obj._pCircle.attr('cy'));
-		// end set creation
-		obj._set = paper.setFinish();
-		// change cursor when hovering set
-		obj._set.attr({opacity: 1, cursor: "move"});
-		// makes the set draggable
-		obj._set.draggable();
-		// return the new object
-		return obj;
-	},
-	// "private" class representing the state.
-	__stateClass: function () {
-		// @todo: methods
-	}
-};
-
-/**  
- * Label Factory: responsible for building label objects
+ * State class
+ * 
+ * @param paper: RaphaelJS canvas object.
+ * @param paramStrText: string
+ * @param paramX: number
+ * @param paramY: number
+ * @param paramRadius: number
  */
-var Label = {
-	/**
-	 * @param paramStrText: string
-	 * @param paramX: number
-	 * @param paramY: number
-	 * @param paramFontSize: number
-	 */
-	build: function(paramStrText, paramX, paramY, paramFontSize) {
-		var obj = new Label.__labelClass();
-		// default values
-		var fSize = (typeof paramFontSize !== 'undefined') ? paramFontSize : App.Config.Pref.fontSize;
-		// object contruction
-		obj._pText = paper.text(paramX, paramY, paramStrText);
-		obj._pText.attr("font-size", App.Config.Pref.fontSize);
-		obj._pText.attr('font-family', App.Config.Pref.fontFamily);
-		obj._pText.attr('fill', App.Config.Pref.strokeColor);	
-		obj._pText.attr('font-size', App.Config.Pref.fontSize);
-		// return the new object
-		return obj;
-	},
-	__labelClass: function () {
-		// @todo: methods
-	}
-};
+function State(paramStrText, paramX, paramY, paramRadius) {
+	// default values
+	this.__x       = (typeof paramX      !== 'undefined')  ? paramX      : App.Config.Pref.xPos;
+	this.__y       = (typeof paramY      !== 'undefined')  ? paramY      : App.Config.Pref.yPos;
+	this.__radius  = (typeof paramRadius !== 'undefined')  ? paramRadius : App.Config.Pref.radius;
+	// starts set, ie Circle (State) + Text (Label)
+	paper.setStart();
+	// create the object
+	this._pCircle = paper.circle(this.__x, this.__y, this.__radius);
+	this._pCircle.attr("stroke-width", App.Config.Pref.strokeWidth);
+	this._pCircle.attr("fill", App.Config.Pref.fillColor);
+	this._pCircle.attr("stroke", App.Config.Pref.strokeColor);
+	this._label = new Label(paramStrText, 
+		this._pCircle.attr('cx'), this._pCircle.attr('cy'));
+	// end set creation
+	this._set = paper.setFinish();
+	// change cursor when hovering set
+	this._set.attr({opacity: 1, cursor: "move"});
+	// makes the set draggable
+	this._set.draggable();
+}
 
-function make_transtition(ox, oy, tx, ty) {
+/**  
+ * Label class
+ * 
+ * @param paramStrText: string
+ * @param paramX: number
+ * @param paramY: number
+ * @param paramFontSize: number
+ */
+function Label(paramStrText, paramX, paramY, paramFontSize) {
+	// default values
+	this.__fSize = (typeof paramFontSize !== 'undefined') ? paramFontSize : App.Config.Pref.fontSize;
+	// object contruction
+	this._pText = paper.text(paramX, paramY, paramStrText);
+	this._pText.attr("font-size", App.Config.Pref.fontSize);
+	this._pText.attr('font-family', App.Config.Pref.fontFamily);
+	this._pText.attr('fill', App.Config.Pref.strokeColor);	
+	this._pText.attr('font-size', this.__fSize);
+}
+
+/**
+ * Transition Class
+ *
+ * @param ox: number
+ * @param oy: number
+ * @param tx: number
+ * @param ty: number
+ *
+ */
+
+function Transition(ox, oy, tx, ty) {
 	// setting middle point
-	var mx = (ox + tx) / 2,
-		my = (oy + ty) / 2,
-		// set paths
-		path = ["M", ox, oy, "S", mx, my, tx, ty];
-		lines = ["M", mx, my, "L", ox, oy, "M", mx, my, "L", tx, ty];
+	this.__mx = (ox + tx) / 2;
+	this.__my = (oy + ty) / 2;
+	// set paths
+	this.__path = ["M", ox, oy, "S", this.__mx, this.__my, tx, ty];
+	this.__lines = ["M", this.__mx, this.__my, "L", ox, oy, "M", this.__mx, this.__my, "L", tx, ty];
 
-	_path_obj = paper.path(path)
-					  .attr({color: "#ffaa44", "stroke-width": 5});
-	_control_obj = paper.circle(mx, my, 5).attr({fill: "#444", stroke: "none"});
-
-	_lines_obj = paper.path(lines)
+	this._lines_obj = paper.path(this.__lines)
 					  .attr({color: "#ffaa44", "stroke-dasharray": "- ", "stroke-width": 1});
 
-	// called when drag starts
-	function start(sx, sy) {
+	this._path_obj = paper.path(this.__path)
+					  .attr({color: "#ffaa44", "stroke-width": 5});
+	this._control_obj = paper.circle(this.__mx, this.__my, 5).attr({fill: "#444", stroke: "none"});
+
+	// register the drag callbacks
+	this._control_obj.drag(this.__move(this), this.__start(this), this.__end(this));
+}
+
+// BEGIN - methods
+
+// called when drag starts
+Transition.prototype.__start = function(obj) {
+	// closure that acts as the move func
+	return function(sx, sy) {
 		// set the origin of movement to the previous center point
 		this.ox = this.attr('cx');
 		this.oy = this.attr('cy');
 		this.attr({opacity: 0.5});
 	}
+}
 
-	// called everytime position changes
-	function move (dx, dy) {
+// called everytime position changes
+Transition.prototype.__move = function(obj) {
+	return function(dx, dy) {
 		// center now moves to origin+distance
-        this.attr({'cx': this.ox + dx, 'cy': this.oy + dy});
-        // the position of our control point has changed, thus register new position.
-        path[4] = this.ox + dx;
-        path[5] = this.oy + dy;
-        lines[1] = lines[7] = this.ox + dx;
-        lines[2] = lines[8] = this.oy + dy;
-        // apply changes to paths.
-        _path_obj.attr({path: path});
-        _lines_obj.attr({path: lines});
+	    this.attr({'cx': this.ox + dx, 'cy': this.oy + dy});
+	    // the position of our control point has changed, thus register new position.
+	    obj.__path[4] = this.ox + dx;
+	    obj.__path[5] = this.oy + dy;
+	    obj.__lines[1] = obj.__lines[7] = this.ox + dx;
+	    obj.__lines[2] = obj.__lines[8] = this.oy + dy;
+	    // apply changes to paths.
+	    obj._path_obj.attr({path: obj.__path});
+	    obj._lines_obj.attr({path: obj.__lines});
 	}
+}
 
-	// called when the drag is over
-	function end () {
+// called when the drag is over
+Transition.prototype.__end = function(obj) {
+	return function () {
 		this.attr({opacity: 1});
 	}
-
-	// register the drag callbacks
-	_control_obj.drag(move, start, end);
-
 }
 
 $(document).ready(function() {
@@ -219,30 +220,8 @@ $(document).ready(function() {
 			paper.setSize($('#svg_canvas_container').width(), $('#svg_canvas_container').height());
 		}
 	});
-/*	$(document).keypress(function(event) {
-		// if we are handling states and 'a' was pressed.
-		console.log('Key pressed.');
-		if (App.Config.Mode == 1 && event.keyCode == 97) {
-			console.log('"a" pressed.');
-			// reset (x,y) position so the system knows it has to capture next click
-			App.Config.Internal.InsertAt.x = -1;
-			App.Config.Internal.InsertAt.y = -1;
-		}
-	}).click(function(event) {
-		// if the system is waiting for a click, ie user has pressed 'a' (x == -1) then insert at click position
-		console.log('Clicked.');
-		if (App.Config.Mode == 1 && App.Config.Internal.InsertAt.x == -1) {
-			var offset = $(this).offset();
-    		App.Config.Internal.InsertAt.x = event.clientX - offset.left;
-    		App.Config.Internal.InsertAt.y = event.clientY - offset.top;
 
-    		console.log('New state at: '+App.Config.Internal.InsertAt.x+", "+App.Config.Internal.InsertAt.y)
-			
-			insert_state(App.Config.Internal.InsertAt.x, App.Config.Internal.InsertAt.y);
-		}
-	});*/
-
-    make_transtition(100, 100, 300, 300);
+    t = new Transition(10, 10, 300, 300);
     
 });
 
