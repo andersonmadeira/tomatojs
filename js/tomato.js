d3334af5d1460deb79705ad3db3b5a263f35e931
andersonmadeira <andersonmadeirawired@gmail.com>
2014-08-26 19:29:37 -0300

    Background rect.
    Now we have a background rect, this will help drawing

--- js/tomato.js
+++ js/tomato.js
@@ -1,8 +1,3 @@
-/*
- * The global canvas used to draw svg
- */
-var paper = null;
-
 /**
  * App config
  */
@@ -20,10 +15,11 @@ var App = {
 			fontSize: 25,
 		},
 		displayControlPoints: true,
-		Internal: {
-			InsertAt: {x: 0, y: 0}
-		},
 		StateCount: 0
+	},
+	Canvas: {
+		paper: null,
+		bgRect: null
 	},
 	insert_state: function() {
 		// if state mode is on
@@ -71,16 +67,16 @@ function State(paramStrText, paramX, paramY, paramRadius) {
 	this.__y       = (typeof paramY      !== 'undefined')  ? paramY      : App.Config.Pref.yPos;
 	this.__radius  = (typeof paramRadius !== 'undefined')  ? paramRadius : App.Config.Pref.radius;
 	// starts set, ie Circle (State) + Text (Label)
-	paper.setStart();
+	App.Canvas.paper.setStart();
 	// create the object
-	this._pCircle = paper.circle(this.__x, this.__y, this.__radius);
+	this._pCircle = App.Canvas.paper.circle(this.__x, this.__y, this.__radius);
 	this._pCircle.attr("stroke-width", App.Config.Pref.strokeWidth);
 	this._pCircle.attr("fill", App.Config.Pref.fillColor);
 	this._pCircle.attr("stroke", App.Config.Pref.strokeColor);
 	this._label = new Label(paramStrText, 
 		this._pCircle.attr('cx'), this._pCircle.attr('cy'));
 	// end set creation
-	this._set = paper.setFinish();
+	this._set = App.Canvas.paper.setFinish();
 	// change cursor when hovering set
 	this._set.attr({opacity: 1, cursor: "move"});
 	// makes the set draggable
@@ -90,7 +86,17 @@ function State(paramStrText, paramX, paramY, paramRadius) {
 	this.__oy = 0;
 	
 	this._set.drag(this.__onMove(this), this.__onStart(this), this.__onEnd(this));
+	this._set.mouseup(this.__onMouseUp);
+	this._set.mousedown(this.__onMouseDown);
 }
+
+State.prototype.__onMouseUp = function() {
+	console.log('Mouse up!');
+};
+
+State.prototype.__onMouseDown = function() {
+	console.log('Mouse Down!');
+};
 
 // note: also a single left mouse click
 State.prototype.__onStart = function (obj) {
@@ -136,7 +142,7 @@ function Label(paramStrText, paramX, paramY, paramFontSize) {
 	// default values
 	this.__fSize = (typeof paramFontSize !== 'undefined') ? paramFontSize : App.Config.Pref.fontSize;
 	// object contruction
-	this._pText = paper.text(paramX, paramY, paramStrText);
+	this._pText = App.Canvas.paper.text(paramX, paramY, paramStrText);
 	this._pText.attr("font-size", App.Config.Pref.fontSize);
 	this._pText.attr('font-family', App.Config.Pref.fontFamily);
 	this._pText.attr('fill', App.Config.Pref.strokeColor);	
@@ -161,21 +167,21 @@ function Transition(symbolStr, ox, oy, tx, ty) {
 	this.__path = ["M", ox, oy, "S", mx, my, tx, ty];
 	this.__lines = ["M", mx, my, "L", ox, oy, "M", mx, my, "L", tx, ty];
 
-	this._lines_obj = paper.path(this.__lines)
+	this._lines_obj = App.Canvas.paper.path(this.__lines)
 				  .attr({color: "#ffaa44", "stroke-dasharray": "- ", "stroke-width": 1});
 
-	this._path_obj = paper.path(this.__path)
+	this._path_obj = App.Canvas.paper.path(this.__path)
 					  .attr({color: "#ffaa44", "stroke-width": 5});
 
 	// aux points
-	this._control_obj2 = paper.circle((this.__path[4] + this.__path[1]) / 2, 
+	this._control_obj2 = App.Canvas.paper.circle((this.__path[4] + this.__path[1]) / 2, 
 									  (this.__path[5] + this.__path[2]) / 2, 5).attr({fill: "#00FF00", stroke: "none"});
-	this._control_obj3 = paper.circle((this.__path[4] + this.__path[6]) / 2, 
+	this._control_obj3 = App.Canvas.paper.circle((this.__path[4] + this.__path[6]) / 2, 
 									  (this.__path[5] + this.__path[7]) / 2, 5).attr({fill: "#0000FF", stroke: "none"});
-	this._control_obj4 = paper.circle((this._control_obj2.attr('cx') + this._control_obj3.attr('cx')) / 2, 
+	this._control_obj4 = App.Canvas.paper.circle((this._control_obj2.attr('cx') + this._control_obj3.attr('cx')) / 2, 
 									  (this._control_obj2.attr('cy') + this._control_obj3.attr('cy')) / 2, 5).attr({fill: "#FF0000", stroke: "none"});
 
-	this._control_obj = paper.circle(mx, my, 5).attr({fill: "#444", stroke: "none"});
+	this._control_obj = App.Canvas.paper.circle(mx, my, 5).attr({fill: "#444", stroke: "none"});
 
 	this._label = new Label(symbolStr, this._control_obj4.attr('cx'), this._control_obj4.attr('cy'));
 
@@ -236,17 +242,31 @@ Transition.prototype.__onEnd = function(obj) {
 
 $(document).ready(function() {
 	// init main objs
-	paper = new Raphael('svg_canvas_container', 794, 394);
+	App.Canvas.paper = new Raphael('svg_canvas_container', 800, 400);
+	// background rect
+	App.Canvas.bgRect = App.Canvas.paper.rect(0, 0, 800, 400).attr({'stroke': 'none', 'fill': '#FAFAFA'}).toBack();
 	// resizes the div container and canvas
 	$('#svg_canvas_container').resizable({
 		resize: function( event, ui ) {
-			paper.setSize($('#svg_canvas_container').width(), $('#svg_canvas_container').height());
+			var w = $('#svg_canvas_container').width(),
+				h = $('#svg_canvas_container').height();
+			App.Canvas.paper.setSize(w, h);
+			App.Canvas.bgRect.attr({'width': w, 'height': h});
 		}
+	});
+
+	$(document).keydown(function(event) {
+		console.log("Key down [code]: "+event.keyCode);
+	});
+
+	$(document).keyup(function(event) {
+		console.log("Key up [code]: "+event.keyCode);
 	});
 
     t = new Transition('Anderson', 10, 10, 300, 300);
     t2 = new Transition('symbol', 200, 200, 300, 100);
     t3 = new Transition('0101011', 102, 230, 30, 50);
+    t3 = new Transition('Foo', 200, 200, 600, 200);
     
 });
 
